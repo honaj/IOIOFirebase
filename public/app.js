ce95f3df2190f8aa6dbcd99464bedc960b999c4e
Johan Hellgren <honajnahoj@gmail.com>
2018-10-04 02:59:32 +0200

    Fixed pushing

--- public/app.js
+++ public/app.js
@@ -1,13 +1,16 @@
-let borrowerName = document.getElementById("borrowerName");
+let amount = document.getElementById("amount");
 let objectToBorrow = document.getElementById("objectToBorrow");
 let sendButton = document.getElementById("sendButton");
 let borrowListTable = document.getElementById("borrowListTable");
 let nameRow = document.getElementById("nameRow");
+let amountRow = document.getElementById("amountRow");
 let objectRow = document.getElementById("objectRow");
 let dateRow = document.getElementById("dateRow");
 let returnedRow = document.getElementById("returnedRow");
-let cells = [];
 let user;
+let cells = [];
+let keys = [];
+let returnButtons = [];
 
 document.addEventListener('DOMContentLoaded', function() {
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
     //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    //Init database
     let config = {
         apiKey: "AIzaSyD6khnayxet1ExZAzmvLr5x29ZiStxopIU",
         authDomain: "ioiolendingsystem.firebaseapp.com",
@@ -26,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
         storageBucket: "gs://ioiolendingsystem.appspot.com"
       };
     firebase.initializeApp(config);
+    
       // Get a reference to the database service
     let database = firebase.database();
     try {
@@ -36,57 +41,73 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error(e);
       document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
     }
-    let realtimeRef = firebase.database().ref('/users');
+    
+    //Get database
+    let realtimeRef = firebase.database().ref();
+    //On startup
     realtimeRef.once('value', function(snapshot) {
         populateTable(snapshot);
     });
+    //On every edit
     realtimeRef.on('value', function(snapshot) {
         populateTable(snapshot);
     });
-    //login();
+    login();
   });
 
-  function login() {
+//Login with Google account
+function login() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
-        .then(result => {
-            user = result.user;
-            document.write(user.displayName);
-        })
-  }
-
-  sendButton.onclick = function() {
-    if(borrowerName.value && objectToBorrow.value) {
-        let today = new Date();
-        let fullDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-        let newLoan = {name: borrowerName.value, object: objectToBorrow.value, date: fullDate, returned: false};
-        firebase.database().ref('users/'+ newLoan.name).set({
-            name: newLoan.name,
-            object: newLoan.object,
-            date: newLoan.date,
-            returned: newLoan.returned
-          });
-        borrowerName.value = "";
-        objectToBorrow.value = "";
-    }
+    .then(result => {
+        user = result.user;
+    })
 }
 
+//Get today's date
+function getDate() {
+    let today = new Date();
+    return today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+}
+
+//Send new loan data to database
+sendButton.addEventListener('click', function() {
+    if(user && objectToBorrow.value && isNaN(objectToBorrow.value) && amount.value && !isNaN(amount.value)) {
+       let newLoan =  firebase.database().ref().push({
+            name: user.displayName,
+            object: objectToBorrow.value,
+            amount: amount.value,
+            date: getDate(),
+            returned: "",
+            });
+        objectToBorrow.value = "";
+        amount.value = "";
+    }
+    else console.log("bad input!");
+});
+
+//Draw database to table
 function populateTable(snapshot) {
-    let returnArray = [];
-        snapshot.forEach(function(childSnapshot) {
-            let item = childSnapshot.val();
-            returnArray.push(item);
-        });
+    let loanArray = [];
+    snapshot.forEach(function(childSnapshot) {
+        keys.push(childSnapshot.key);
+        loanArray.push(childSnapshot.val());
+    });
+    for(key of keys) {
+        console.log(key);
+    }
     for(cell of cells) {
         cell.remove();
     }
-    for(item of returnArray) {
+    for(item of loanArray) {
         //console.log(item);
         cells.push(nameRow.insertCell().appendChild(document.createTextNode(item.name)));
         cells.push(objectRow.insertCell().appendChild(document.createTextNode(item.object)));
+        cells.push(amountRow.insertCell().appendChild(document.createTextNode(item.amount)));
         cells.push(dateRow.insertCell().appendChild(document.createTextNode(item.date)));
-        let buttonCell = returnedRow.insertCell();
-        cells.push(buttonCell.appendChild(document.createTextNode(item.returned)));
-        //document.createElement("button")
+        cells.push(returnedRow.insertCell().appendChild(document.createTextNode(item.returned)));
+        //let buttonCell = returnedRow.insertCell();
+        //let returnButton = cells.push(buttonCell.appendChild(document.createElement("button")));
+        //returnButton.style.value = "test";
     }
 } 
